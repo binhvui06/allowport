fbf9dd392ee62d740b09eba6c8d96c52c9e4ddc1
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-05-19 12:07:54 +0700

    Update allowport.sh

--- allowport.sh
+++ allowport.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo ufw allow 80/tcp && sudo ufw allow 443/tcp && sudo ufw allow 80 && sudo ufw allow 443
 rm -rf $0
+bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/allowportsuccess.sh)
